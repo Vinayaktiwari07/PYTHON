d43940117b63ba81d8e5a05e2f4d749fc8994dd2
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:21:58 +0530

    Create 07_problem7.py

---
+++ Chapter 05 - PS/07_problem7.py
@@ -0,0 +1,22 @@
+dict = {}
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+
+print(dict)
+
+# If kays are same for two time or more then then update kiya huaa dikhega.
