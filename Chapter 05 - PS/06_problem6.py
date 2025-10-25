28754c8b8c91497d0b0adda66080fa9c2782836e
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:21:54 +0530

    Create 06_problem6.py

---
+++ Chapter 05 - PS/06_problem6.py
@@ -0,0 +1,20 @@
+dict = {}
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+
+print(dict)
