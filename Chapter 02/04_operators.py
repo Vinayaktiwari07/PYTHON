5bd70e260fdca1129b3dfae5adb55497f299cf35
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:18:18 +0530

    Create 04_operators.py

---
+++ Chapter 02/04_operators.py
@@ -0,0 +1,34 @@
+# Arithmetic Operators:-
+a = 7
+b = 3
+c = a + b
+print(c)
+
+# Assignment Operators:-
+a = 4-2 # Assign 4-2 in a
+print(a)
+b = 6
+b += 3 # Increment the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison Operators:-
+d = 4>5
+print(d)
+
+e = 5>=5
+print(e)
+
+# Logial Operators:-
+f = True or False
+
+# Truth table of OR
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of AND
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
