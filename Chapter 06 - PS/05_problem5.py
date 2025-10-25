253dc1fabcedf2a689913c4c8abb546f237ac64e
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:22:51 +0530

    Create 05_problem5.py

---
+++ Chapter 06 - PS/05_problem5.py
@@ -0,0 +1,11 @@
+name = ["Vinayak", "vinayak", "Alok", "alok", "Rupesh", "rupesh", "Govind", "govind", "Shekhar", "shekhar", "Rudra", "rudra", "Dev", "dev"]
+
+a = input("Enter your name: ")
+
+if(a in name):
+    print("Yes, your are in list")
+
+else:
+    print("No, You are not in list")
+
+   
