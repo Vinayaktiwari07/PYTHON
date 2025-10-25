b97a1c1c34f33b0885af19db6fcf52cb695f752b
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:28:32 +0530

    Create 01_class.py

---
+++ Chapter 10/01_class.py
@@ -0,0 +1,10 @@
+class Employee:
+    name = "Harry"
+    language = "py"
+    salary = 1200000
+
+harry = Employee()
+print(harry.language, harry.salary)
+
+rohit = Employee()
+print(rohit.language, rohit.salary)
