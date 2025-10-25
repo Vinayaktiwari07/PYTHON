9e7080b4ad5051b2de8eef78c0129250c571d866
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:20:47 +0530

    Create 04_escape_seq.py

---
+++ Chapter 03/04_escape_seq.py
@@ -0,0 +1,9 @@
+a = "Vinayak is a good boy\nbut not a bad boy" # \n make sentences in a new line
+
+print(a)
+
+b = "Vinayak is a good boy\tbut not a bad boy" # \t make a tab between sentences.
+print(b)
+
+c = "Vinayak is a good \"boy\" but not a bad boy"  # \" make double-qoute in sentences.
+print(c)
