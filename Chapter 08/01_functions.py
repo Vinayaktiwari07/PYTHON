473538520d9aad3925ff22f73ee2a3acfc0068d6
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:25:16 +0530

    Create 01_functions.py

---
+++ Chapter 08/01_functions.py
@@ -0,0 +1,6 @@
+# Function Definition:
+def func():
+    print("Hello")
+
+# Function Call:
+func()  #hm jb jb func() ko call krenge ye ye apne under ki operation ko krega. like yaha pr hello print krwaya ja rha hai.
