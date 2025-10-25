b3da5ffbb2ce8fbf7547cfdd98a0c37fbc6a228d
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:24:23 +0530

    Create 05_for_loop_iterate.py

---
+++ Chapter 07/05_for_loop_iterate.py
@@ -0,0 +1,16 @@
+# For loop with lists 
+l = [1, 4, 3, 23, 43, 21]
+
+for i in l:
+    print(i)
+
+
+# For loop with tuples
+t = (2, 9, 90, 45)
+for i in t:
+    print(i)
+
+# For loop with strings
+s = "Vinayak"
+for i in s:
+    print(i)
