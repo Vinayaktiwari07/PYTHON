68502b1b3af8e7183406b253b54dc0c1f16b9ee7
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:24:32 +0530

    Create 07_break_and_continue.py

---
+++ Chapter 07/07_break_and_continue.py
@@ -0,0 +1,9 @@
+for i in range(100):
+    if(i == 41):
+        break   # Ye loop ko exit kr dega jaise hi isko break wali value milegi.
+    print(i)
+
+for i in range(50):
+    if(i == 20):
+        continue  # Yaha jo value pass ki jayegi sirf usko print nhi krega
+    print(i)
