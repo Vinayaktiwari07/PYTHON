2b62770390975c8c4566b710b0e93c5df10700d5
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:22:02 +0530

    Create 08_problem8.py

---
+++ Chapter 05 - PS/08_problem8.py
@@ -0,0 +1,23 @@
+dict = {}
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+key = input("Enter your name: ")
+value = input("Enter your favourite language: ")
+dict.update({key: value})
+
+
+print(dict)
+
+
+# If the value are same then no issue it print.
