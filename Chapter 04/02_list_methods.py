722e65889ac17acfd36f27c93ab22afb8bf4326e
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:21:18 +0530

    Create 02_list_methods.py

---
+++ Chapter 04/02_list_methods.py
@@ -0,0 +1,22 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+
+friends.append("Vinayak") # append means at the end add cheeje add krna
+print(friends)
+
+l1 = [1, 23, 43, 4, 3, 5]
+l1.sort() # it sort the list in ascending order
+print(l1)
+
+l1.reverse()
+print(l1)
+
+l1.insert(2, 99) # 2 index pr 99 value add krdega
+print(l1)
+
+l1.pop(1) # Yaha index 1 pr jo bhi rhega wo delete kr dega
+print(l1)
+
+
+l1.remove(43) # Ye jo value put krenge list se usko remove kr dega.
+print(l1)
