83b149dc4cfe1f88e23a36db42c5b614f6c51326
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:23:00 +0530

    Create 07_problem7.py

---
+++ Chapter 06 - PS/07_problem7.py
@@ -0,0 +1,7 @@
+post = input("Enter your post: ")
+
+if("Harry".lower() in post.lower()):  # Yala pr .lower lgane se word ko lower form me change kr dega or post me bhi word ko lower kr dega.
+    print("This post is talking about Harry")
+
+else:
+    print("This post is not talking about Harry")
