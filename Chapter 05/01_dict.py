898ad74a1462cae9f6226c4381465037e4482fcf
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:22:11 +0530

    Create 01_dict.py

---
+++ Chapter 05/01_dict.py
@@ -0,0 +1,10 @@
+d = {} # Empty Dictionary
+marks = {
+    "Vinayak": 100,
+    "Rupesh": 99,
+    "Alok": 99
+}
+
+print(marks, type(marks)) # Dictionaty  'key:value' express krta hai.
+
+print(marks["Vinayak"])
