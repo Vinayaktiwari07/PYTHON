e362a5c0f25cf05620921c641ca73290ee4e50cd
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-25 10:21:28 +0530

    Create 04_tuple_methods.py

---
+++ Chapter 04/04_tuple_methods.py
@@ -0,0 +1,10 @@
+b = (1, 34, 7765, "Vinayak", False, 34)
+print(b)
+
+no = b.count(34) # ye value ko count krega ki tuple me kitne times wo values aa rha hai.
+print(no)
+
+
+i = b.index(34) # Ye first occurance index find krega value ka 
+print(i)
+print(len(b))
